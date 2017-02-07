170118ce9bbdeea60852ee01f58445f05019d7e8
Ashley Hsiao <aiyihsiao@gmail.com>
2017-02-07 15:17:23 -0800

    Add doc for timestamps

--- app/Task.php
+++ app/Task.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * Enables/disables created_at and updated_at columns to exist on the table.
+     *
+     * @var bool
+     */
     public $timestamps = false;
+
 }
