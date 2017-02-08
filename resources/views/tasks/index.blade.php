8a5e531a1e769f248ceaabc4cb405c7a03733886
Ashley Hsiao <aiyihsiao@gmail.com>
2017-02-07 18:33:57 -0800

    List out the individual tasks

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts.base')
 
 @section('content')
-    <ul>
-        @foreach ($tasks as $task)
-            <li>
-                {{ $task->title }}
-            </li>
-        @endforeach
-    </ul>
+  <h1>Tasks</h1>
+  
+  <table class="table">
+    <thead class="thead-inverse">
+      <tr>
+        <th>Task ID</th>
+        <th>Title</th>
+        <th>Status</th>
+        <th>Parent Task</th>
+        <th></th>
+      </tr>
+    </thead>
+
+    <tbody>
+      @foreach ($tasks as $task)
+        <tr>
+          <td>{{ $task->id }}</td>
+          <td>{{ $task->title }}</td>
+          <td>{{ $task->status }}</td>
+          <td>{{ $task->parent_id }}</td>
+          <td></td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
 @endsection
