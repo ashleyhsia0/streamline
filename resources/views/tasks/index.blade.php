e8966dee04efca8a13929e1e9300b425b2586fdf
Ashley Hsiao <aiyihsiao@gmail.com>
2017-02-07 17:37:11 -0800

    Create base template

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Tasks</title>
-    </head>
-    <body>
-        <ul>
-            @foreach ($tasks as $task)
-                <li>
-                    {{ $task->title }}
-                </li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@extends('layouts.base')
+
+@section('content')
+    <ul>
+        @foreach ($tasks as $task)
+            <li>
+                {{ $task->title }}
+            </li>
+        @endforeach
+    </ul>
+@endsection
