707864ef61daecb1b5e48890bd2dc0eb8b762ad8
Ashley Hsiao <aiyihsiao@gmail.com>
2017-02-08 17:23:16 -0800

    Change back to 4 spaces for indentation

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Tasks</title>
+        <title>Tasks</title>
 
-    <link rel="stylesheet" href="/css/app.css">
-    @yield('stylesheets')
-  </head>
+        <link rel="stylesheet" href="/css/app.css">
+        @yield('stylesheets')
+    </head>
 
-  <body>
-    <div class="container">
-      @yield('content')
-    </div>
+    <body>
+        <div class="container" id="app">
+            @yield('content')
+        </div>
 
-    <script src="/js/app.js"></script>
-    @yield('scripts')
-
-  </body>
+        <script src="/js/app.js"></script>
+        @yield('scripts')
+    </body>
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,39 +4,39 @@
 @endsection
 
 @section('content')
-  <h1>Tasks</h1>
+    <h1>Tasks</h1>
 
-  <table class="table">
-    <thead class="thead-inverse">
-      <tr>
-        <th>Task ID</th>
-        <th>Title</th>
-        <th>Status</th>
-        <th>Parent Task</th>
-        <th></th>
-      </tr>
-    </thead>
+    <table class="table">
+        <thead class="thead-inverse">
+            <tr>
+                <th>Task ID</th>
+                <th>Title</th>
+                <th>Status</th>
+                <th>Parent Task</th>
+                <th></th>
+            </tr>
+        </thead>
 
-    <tbody>
-      @foreach ($tasks as $task)
-        <tr>
-          <td>{{ $task->id }}</td>
-          <td>{{ $task->title }}</td>
-          <td>
-            @if ($task->status == 0)
-              IN PROGRESS
-            @elseif ($task->status == 1)
-              DONE
-            @else
-              COMPLETED
-            @endif
-          </td>
-          <td>{{ $task->parent_id }}</td>
-          <td></td>
-        </tr>
-      @endforeach
-    </tbody>
-  </table>
+        <tbody>
+            @foreach ($tasks as $task)
+            <tr>
+                <td>{{ $task->id }}</td>
+                <td>{{ $task->title }}</td>
+                <td>
+                    @if ($task->status == 0)
+                        IN PROGRESS
+                    @elseif ($task->status == 1)
+                        DONE
+                    @else
+                        COMPLETED
+                    @endif
+                </td>
+                <td>{{ $task->parent_id }}</td>
+                <td></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
 @section('scripts')
